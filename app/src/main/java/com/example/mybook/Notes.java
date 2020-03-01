16dfd11df8d7b99c91c0f894d80664c44a612a2f
First Commit <farhanasharmin780@yahoo.com>
2020-03-02 00:50:45 +0600

    MyBook_Notepad - First member commit

--- app/src/main/java/com/example/mybook/Notes.java
+++ app/src/main/java/com/example/mybook/Notes.java
@@ -1,27 +1,63 @@
 package com.example.mybook;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Notes extends AppCompatActivity {
 
    static ArrayList<String> notes = new ArrayList<>();
+   static ArrayAdapter arrayAdapter;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_note_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        if (item.getItemId()== R.id.add_note){
+            Intent intent = new Intent(getApplicationContext(), NoteEditor.class);
+            startActivity(intent);
+            return true;
+        }
+        return false;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
 
         ListView ListView = (ListView) findViewById(R.id.showDataLV);
-        notes.add("Example Notes");
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, notes);
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.mybook", Context.MODE_PRIVATE);
+        HashSet<String> set = (HashSet<String>) sharedPreferences.getStringSet("Notes", null);
+        if (set == null) {
+            notes.add("Example Notes");
+        }else{
+            notes = new ArrayList<>(set);
+        }
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, notes);
         ListView.setAdapter(arrayAdapter);
 
         ListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -34,5 +70,34 @@ public class Notes extends AppCompatActivity {
             }
         });
 
+        ListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+
+                final int itemToDelete = position;
+
+                new AlertDialog.Builder(Notes.this)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle("Are You Sure?")
+                        .setMessage("Do you want to delete this note?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                notes.remove(itemToDelete);
+                                arrayAdapter.notifyDataSetChanged();
+                                SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.mybook", Context.MODE_PRIVATE);
+
+                                HashSet<String> set = new HashSet(Notes.notes);
+                                sharedPreferences.edit().putStringSet("notes", set).apply();
+                            }
+                        }
+                        )
+                        .setNegativeButton("No", null)
+                        .show();
+
+
+                return true;
+            }
+        });
     }
 }
